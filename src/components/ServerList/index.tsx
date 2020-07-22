c564b94e33ef298a3d1a1def0d944ce3f6099a07
elascalla <erick.lascalla@gmail.com>
2020-07-21 22:19:00 -0400

    Initial commit.

---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { Container, ServerButton, Separator } from './styles';
+
+const ServerList: React.FC = () => {
+  return (
+    <Container>
+      <ServerButton />
+
+      <Separator />
+
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+
+    </Container>
+  )
+}
+
+export default ServerList;
