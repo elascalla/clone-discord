ad3dacc78069b7d742a857efb85f83f26f39457d
Erick Lascalla <erick.lascalla@gmail.com>
2020-07-24 00:38:59 -0400

    Feat: Create UserList

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -1,13 +1,48 @@
-import React from 'react';
+import React from "react";
 
-import { Container } from './styles';
+import { Container, Role, User, Avatar } from "./styles";
+
+interface UserProps {
+  nickName: string;
+  isBot?: boolean;
+}
+
+const UserRow: React.FC<UserProps> = ({ nickName, isBot }) => {
+  return (
+    <User>
+      <Avatar className={isBot ? "bot" : ""} />
+
+      <strong>{nickName}</strong>
+
+      {isBot && <span>Bot</span>}
+    </User>
+  );
+};
 
 const UserList: React.FC = () => {
   return (
     <Container>
-      
+      <Role> Disponível - 1 </Role>
+      <UserRow nickName="Erick Lascalla" />
+
+      <Role> Offline - 15 </Role>
+      <UserRow nickName="Davison André Oliveira" isBot />
+      <UserRow nickName="Fulano de tal" />
+      <UserRow nickName="Fulano de tal" />
+      <UserRow nickName="Fulano de tal" />
+      <UserRow nickName="Fulano de tal" />
+      <UserRow nickName="Fulano de tal" />
+      <UserRow nickName="Fulano de tal" />
+      <UserRow nickName="Fulano de tal" />
+      <UserRow nickName="Fulano de tal" />
+      <UserRow nickName="Fulano de tal" />
+      <UserRow nickName="Fulano de tal" />
+      <UserRow nickName="Fulano de tal" />
+      <UserRow nickName="Fulano de tal" />
+      <UserRow nickName="Fulano de tal" />
+      <UserRow nickName="Fulano de tal" />
     </Container>
-  )
-}
+  );
+};
 
 export default UserList;
