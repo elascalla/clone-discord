8605aedcaa0d797f38c7a1c176fa403717431e36
elascalla <erick.lascalla@gmail.com>
2020-07-28 22:41:53 -0400

    Feat: Create Component ChannelMessage

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,11 +1,31 @@
 import React from "react";
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from "./styles";
+import ChannelMessage from "../ChannelMessage";
 
 const ChannelData: React.FC = () => {
   return (
     <Container>
-      <Messages />
+      <Messages>
+        <ChannelMessage
+          author="Erick Lascalla"
+          date="28/07/2020"
+          content="Testando as mensagens"
+        />
+
+        {/* <ChannelMessage
+          author="Davison Oliveira"
+          date="28/07/2020"
+          content={
+            <>
+              <Mention>@Erick Lascalla</Mention>, testando o funcionamento da
+              mention.
+            </>
+          }
+          hasMention
+          isBot
+        /> */}
+      </Messages>
 
       <InputWrapper>
         <Input type="text" placeholder="Conversar em #chat-livre" />
